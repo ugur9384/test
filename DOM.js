370fc317616a2717e04a26e5b4bc1f036ba1a74e
Ugur <ugursozer77@gmail.com>
2025-02-23 12:58:08 +0300

    A little Learning,time for Projects

--- DOM.js
+++ DOM.js
@@ -1,3 +1,35 @@
-/* window.onload = ()=> {
+function iAmClicked(me){
+    me.innerHTML = "I am clicked";
+}
+/*   append 
+window.onload = ()=> {
     document.querySelector("#test").append("world");
 } */
+/* change the text fully
+  window.onload = ()=> {
+    document.querySelector("#test").innerHTML=("Hello world");
+}
+my_button= document.querySelector("#myButton");
+my_button.addEventListener("click",()=>{
+  document.querySelector("#test").innerHTML=("Hello world");
+}) */
+/* propagation
+
+document.getElementById("myDiv").addEventListener("click",()=>{
+  console.log("this");
+})
+document.querySelector("#test").addEventListener("click",()=>{
+  console.log("test clicked");
+})
+*/
+/*  why not just use onClick
+it overwrites the previous onclick
+document.getElementById("myButton").onclick = ()=>{
+  console.log("clicked");
+}
+document.getElementById("myButton").onclick = ()=>{
+  console.log("clicked again");
+}
+*/ 
+/* Project time
+*/
